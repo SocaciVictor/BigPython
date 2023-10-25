41c06456eb2878a823977352a17a87b7a8a3302b
Stemi13 <teodor.stamate@student.unitbv.ro>
2023-10-25 16:34:39 +0300

    Add class Game 1

--- TwixtGame/TwixtGame/main.cpp
+++ TwixtGame/TwixtGame/main.cpp
@@ -1,32 +1,10 @@
-﻿#include <iostream>
-#include<SFML/Graphics.hpp>
+﻿#include "All_Include_File.h"
 
 int main()
 {
-    std::cout << "Stemi Work Her\n";
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Setarea culorii unui cerc in SFML");
-
-    // Crearea unui obiect CircleShape
-    sf::CircleShape circle(50); // Raza cercului
-
-    // Setarea poziției cercului
-    circle.setPosition(375, 275); // Poziția x, y
-
-    // Setarea culorii cercului (în acest caz, verde)
-    circle.setFillColor(sf::Color::Green); // Setarea culorii
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-        }
-
-        window.clear();
-        window.draw(circle); // Desenarea cercului
-        window.display();
-    }
+    std::cout << "Stemi main\n";
+   
+    
     return 0;
 }
 
