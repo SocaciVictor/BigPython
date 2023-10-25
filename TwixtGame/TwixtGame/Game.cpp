25e5c5014312808203dcbddf834e358bc64d25b8
persanu13 <teodor.stamate@student.unitbv.ro>
2023-10-25 20:19:42 +0300

    work class board

--- TwixtGame/TwixtGame/Game.cpp
+++ TwixtGame/TwixtGame/Game.cpp
@@ -7,8 +7,8 @@ void Game::initVariable()
 
 void Game::initWindow()
 {
-	this->videoMode.width = 800;	 // Window width
-	this->videoMode.height = 600;    //Window height
+	this->videoMode.width = 1000;	 // Window width
+	this->videoMode.height = 800;    //Window height
 	this->window = new sf::RenderWindow(this->videoMode, "TwixT BigPython", sf::Style::Close | sf::Style::Titlebar);
 }
 
@@ -56,48 +56,16 @@ void Game::update()
 
 void Game::render()
 {
-	int line = 24;
-	int colum = 24;
-	//Board
-	sf::RectangleShape square(sf::Vector2f(520, 520));
-	square.setPosition(100, 50);
-	square.setFillColor(sf::Color(192, 192, 192, 255));
+	sf::CircleShape circle(50);
+	std::cout << circle.getPosition().x<<"\n";
+		
+	circle.setFillColor(sf::Color(192, 192, 192, 255));
 
-	sf::RectangleShape line1(sf::Vector2f(430, 3));
-	line1.setPosition(148, 91.5);
-	line1.setFillColor(sf::Color::Red);
-
-	sf::RectangleShape line2(sf::Vector2f(430, 3));
-	line2.setPosition(148, 531.5);
-	line2.setFillColor(sf::Color::Red);
-
-	sf::RectangleShape line3(sf::Vector2f(3, 430));
-	line3.setPosition(141.5, 97.5);
-	line3.setFillColor(sf::Color::Black);
-
-	sf::RectangleShape line4(sf::Vector2f(3, 430));
-	line4.setPosition(581.5, 97.5);
-	line4.setFillColor(sf::Color::Black);
 	this->window->clear();
 
 	//Render stuff
 
-	this->window->draw(square);
-	this->window->draw(line1);
-	this->window->draw(line2);
-	this->window->draw(line3);
-	this->window->draw(line4);
-
-	for (int i = 0; i < line; i++) {
-		for (int j = 0; j < colum; j++) {
-			if ((i != 0 || j != 0) && (i != 23 || j != 23) && (i != 23 || j != 0) && (i != 0 || j != 23)) {
-				sf::CircleShape circle(3);
-				circle.setFillColor(sf::Color(128, 128, 128, 255));
-				circle.setPosition(i * 20 + 130, j * 20 + 80);
-				this->window->draw(circle);
-			}
-		}
-	}
+	this->window->draw(circle);
 
 	//Display stuff
 	this->window->display();
