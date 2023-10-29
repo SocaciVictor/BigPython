0e972652d728cff44906c253ba8e3d3147178758
persanu13 <teodor.stamate@student.unitbv.ro>
2023-10-29 09:45:56 +0200

    work on class Table

--- TwixtGame/TwixtGame/Table.h
+++ TwixtGame/TwixtGame/Table.h
@@ -1,6 +1,17 @@
 #pragma once
+#include<string>
+
 class Table
 {
+private:
+	int height;
+	int width;
+	std::string color;
+	
+
+public:
+	Table(int height,int width,std::string color);
+	~Table();
 
 };
 
