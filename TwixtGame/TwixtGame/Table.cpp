a8eca06f3d3ac0a872d90fd08d0aad38f164304c
persanu13 <teodor.stamate@student.unitbv.ro>
2023-10-29 09:49:15 +0200

    work on class table

--- TwixtGame/TwixtGame/Table.cpp
+++ TwixtGame/TwixtGame/Table.cpp
@@ -1,1 +1,36 @@
 #include "Table.h"
+
+Table::Table(int height, int width, std::string color)
+{
+}
+
+Table::~Table()
+{
+}
+
+void Table::setHeight(int height)
+{
+}
+
+void Table::setWidth(int width)
+{
+}
+
+void Table::setColor(std::string color)
+{
+}
+
+int Table::getHeight()
+{
+	return 0;
+}
+
+int Table::getWidth()
+{
+	return 0;
+}
+
+std::string Table::getColor()
+{
+	return std::string();
+}
