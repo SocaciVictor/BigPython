cd041c29f329ed4f315fc140184a41a63b104461
persanu13 <teodor.stamate@student.unitbv.ro>
2023-10-29 09:50:56 +0200

    work on class talbe 3

--- TwixtGame/TwixtGame/Table.cpp
+++ TwixtGame/TwixtGame/Table.cpp
@@ -2,6 +2,9 @@
 
 Table::Table(int height, int width, std::string color)
 {
+	this->height = height;
+	this->width = width;
+	this->color = color;
 }
 
 Table::~Table()
@@ -10,10 +13,12 @@ Table::~Table()
 
 void Table::setHeight(int height)
 {
+	this->height = height;
 }
 
 void Table::setWidth(int width)
 {
+	this->width = width;
 }
 
 void Table::setColor(std::string color)
