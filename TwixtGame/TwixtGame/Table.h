5e490af71346b89019ddc7e9367a0195d9d92ad2
persanu13 <teodor.stamate@student.unitbv.ro>
2023-10-29 09:48:19 +0200

    work on class Table 2

--- TwixtGame/TwixtGame/Table.h
+++ TwixtGame/TwixtGame/Table.h
@@ -12,6 +12,11 @@ private:
 public:
 	Table(int height,int width,std::string color);
 	~Table();
-
+	void setHeight(int height);
+	void setWidth(int width);
+	void setColor(std::string color);
+	int getHeight();
+	int getWidth();
+	std::string getColor();
 };
 
