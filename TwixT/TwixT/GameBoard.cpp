2383cceed9b9f4ccc0c28850354bb0df7ab5be6a
SocaciVictor <victor.socaci@student.unitbv.ro>
2023-11-09 01:26:41 +0200

    Constructor cu parametrii de culoare si size.

--- TwixT/TwixT/GameBoard.cpp
+++ TwixT/TwixT/GameBoard.cpp
@@ -29,3 +29,17 @@ GameBoard::GameBoard(int size, QWidget* parent) : QWidget(parent)
 	styleBoard();
 	addBases();
 }
+
+GameBoard::GameBoard(int size, QColor backround_color, QWidget* parent) : QWidget(parent)
+{
+	this->size = size;
+	this->lines = 10;
+	this->columns = 10;
+	this->size_width = this->size * (this->columns + 1);
+	this->size_height = this->size * (this->lines + 1);
+	this->coordinates = QPoint(this->size_width / 2, this->size_height / 2);
+	this->background_color = backround_color;
+
+	styleBoard();
+	addBases();
+}
