e3f13f112520471945475052ecf9d2f2f22a72ae
SocaciVictor <victor.socaci@student.unitbv.ro>
2023-11-09 01:24:43 +0200

    Constructor default

--- TwixT/TwixT/GameBoard.cpp
+++ TwixT/TwixT/GameBoard.cpp
@@ -1,9 +1,19 @@
 #include "GameBoard.h"
+#include "Base.h"
+#include<QPainter>
 
-GameBoard::GameBoard(QWidget *parent)
-	: QWidget(parent)
+GameBoard::GameBoard(QWidget* parent) : QWidget(parent)
 {
-	ui.setupUi(this);
+	this->size = 20;
+	this->lines = 10;
+	this->columns = 10;
+	this->size_width = this->size * (this->columns + 1);
+	this->size_height = this->size * (this->lines + 1);
+	this->coordinates = QPoint(this->size_width / 2, this->size_height / 2);
+	this->background_color = QColor("blue");
+
+	styleBoard();
+	addBases();
 }
 
 GameBoard::~GameBoard()
