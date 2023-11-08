1bf5c3659c4b1793fdec635bb5449c6a4e592a4b
persanu13 <teodor.stamate@student.unitbv.ro>
2023-11-09 00:34:37 +0200

    lucrez la constructori

--- TwixT/TwixT/MainWindow.cpp
+++ TwixT/TwixT/MainWindow.cpp
@@ -1,27 +1,12 @@
 #include "MainWindow.h"
-#include <QPainter>
 
-MainWindow::MainWindow(int width, int height, QString color, QWidget* parent)
+//Constructori
+MainWindow::MainWindow(QWidget* parent)
 {
-	this->resize(width, height);
-    this->setFixedSize(this->size());
-    QPalette palette;
-    QColor backgroundColor(color);
-    palette.setColor(QPalette::Window, backgroundColor);
-    this->setPalette(palette);
-}
+	this->size_width = 800;
+	this->size_height = 600;
+	this->background_color = "#ffffff";
 
-MainWindow::~MainWindow()
-{}
-
-void MainWindow::paintEvent(QPaintEvent*)
-{
-    //Desenat Dreptunghiuri
-    QPainter painter(this);
-    painter.setRenderHint(QPainter::Antialiasing);
-
-    painter.setBrush(QColor("#DD125C"));
-    painter.drawRect(200, 200, 100, 100);
-    painter.setBrush(QColor("#12DDD1"));
-    painter.drawRect(50, 50, 100, 100);
+	styleMainWindow();
+	displayChildren();
 }
