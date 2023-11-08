63a919fb3d2f99142a5c74630824b6afe67c321b
SocaciVictor <victor.socaci@student.unitbv.ro>
2023-11-09 01:27:14 +0200

    Constructor cu parametrii de linii si coloane.

--- TwixT/TwixT/GameBoard.cpp
+++ TwixT/TwixT/GameBoard.cpp
@@ -43,3 +43,17 @@ GameBoard::GameBoard(int size, QColor backround_color, QWidget* parent) : QWidge
 	styleBoard();
 	addBases();
 }
+
+GameBoard::GameBoard(int lines, int columns, QWidget* parent) : QWidget(parent)
+{
+	this->size = 20;
+	this->lines = lines;
+	this->columns = columns;
+	this->size_width = this->size * (this->columns + 1);
+	this->size_height = this->size * (this->lines + 1);
+	this->coordinates = QPoint(this->size_width / 2, this->size_height / 2);
+	this->background_color = QColor("blue");
+
+	styleBoard();
+	addBases();
+}
