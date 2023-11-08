cac4981459a69859fbacebeae2ad08ef4370f7b3
David-UniTBv <david.secareanu@student.unitbv.ro>
2023-11-09 01:08:42 +0200

    Implementat functie stilizare clasa Base

--- TwixT/TwixT/Base.cpp
+++ TwixT/TwixT/Base.cpp
@@ -14,3 +14,15 @@ Base::Base(uint16_t radius, uint16_t poz_x, uint16_t poz_y, QColor background_co
 
 Base::~Base()
 {}
+
+void Base::styleBase()
+{
+	setFixedSize(radius * 2, radius * 2);
+	//mut Base astfel incat sa aiba mijlocul fix in punctul coordinates
+	move(coordinates.x() - radius, coordinates.y() - radius);
+
+	QString border = "border-radius: " + QString::number(radius) + "px;border: none";
+	QString backgroundColor = "background-color:" + background_color.name() + ";";
+
+	setStyleSheet(backgroundColor + border);
+}
