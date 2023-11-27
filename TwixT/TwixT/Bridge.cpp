ac6bc04a83626be16c47f18d51f6cb03db6a5a5c
persanu13 <teodor.stamate@student.unitbv.ro>
2023-11-27 02:02:49 +0200

    Am adaugat functia de calculare a unghiului pentru bridge

--- TwixT/TwixT/Bridge.cpp
+++ TwixT/TwixT/Bridge.cpp
@@ -1,4 +1,17 @@
-#include "Bridge.h"
+﻿#include "Bridge.h"
+
+
+double Bridge::calculUnghi(const QPoint& point1, const QPoint& point2)
+{
+	// Calculul coordonatelor relative
+	double deltaX = point2.x() - point1.x();
+	double deltaY = point2.y() - point1.y();
+
+	// Calculul unghiului folosind atan2
+	double unghiRad = atan2(deltaY, deltaX);
+
+	return 0.0f;
+}
 
 Bridge::Bridge(Pillar* start, Pillar* end, QColor color, QWidget* parent) :
 	Piece{color,parent}
@@ -8,14 +21,13 @@ Bridge::Bridge(Pillar* start, Pillar* end, QColor color, QWidget* parent) :
 	paintEvent(nullptr);
 }
 void Bridge::paintEvent(QPaintEvent*){
-	//setFixedSize(radius * 2, radius * 2);
-	////mut Base astfel incat sa aiba mijlocul fix in punctul coordinates
-	//move(coordinates.x() - radius, coordinates.y() - radius);
+	setFixedSize(50, 50);
+	//mut Base astfel incat sa aiba mijlocul fix in punctul coordinates
+	move(200, 200);
 
-	//QString border = "border-radius: " + QString::number(radius) + "px;border: none;";
-	//QString backgroundColor = "background-color:" + background_color.name() + ";";
-
-	//setStyleSheet(backgroundColor + border);
-	//show();
+	QString backgroundColor = "background-color:" + color.name() + ";";
+	QString rotate = "transform: rotate(45deg);";
+	setStyleSheet(backgroundColor + rotate);
+	show();
 }
 Pillar* Bridge::save_pillar = nullptr;
--- TwixT/TwixT/Bridge.h
+++ TwixT/TwixT/Bridge.h
@@ -5,9 +5,11 @@ class Bridge : public Piece
 {
 private:
 	QPoint p_start, p_end;
+	double calculUnghi(const QPoint& point1, const QPoint& point2);
 public:
 	static Pillar* save_pillar;
 	Bridge() = default;
 	Bridge(Pillar* start, Pillar* end,QColor color,QWidget* parent);
 	void paintEvent(QPaintEvent*) override;
+	
 };
