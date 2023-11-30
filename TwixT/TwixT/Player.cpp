51c6c6f29c4b77d0ef3047f6a6335ad9942964f4
persanu13 <teodor.stamate@student.unitbv.ro>
2023-11-30 16:55:49 +0200

    End class Player

--- TwixT/TwixT/Player.cpp
+++ TwixT/TwixT/Player.cpp
@@ -1,1 +1,20 @@
 #include "Player.h"
+
+Player::Player(std::uint8_t number_pillars, std::uint8_t number_bridges, PieceColor color) :
+	m_number_pillars{ number_pillars }, m_number_bridges{ number_bridges }, m_color{ color }, m_moved{ false }
+{}
+
+PieceColor Player::getColor() const noexcept
+{
+	return m_color;
+}
+
+const bool& Player::getMoved() const noexcept
+{
+	return m_moved;
+}
+
+void Player::setMoved(bool moved)
+{
+	m_moved = moved;
+}
