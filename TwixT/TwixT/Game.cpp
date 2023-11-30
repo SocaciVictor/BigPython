3b4e1d50bf6685e0cfcc8eb690acbc66bf72e18c
GrandPaPaRa <tomaghghgh@gmail.com>
2023-11-30 16:47:01 +0200

    update game class

---
+++ TwixT/TwixT/Game.cpp
@@ -0,0 +1,39 @@
+#include "Game.h"
+
+Game::Game() :
+	m_board{ 24,24,this },
+	m_player1{ std::make_shared<Player>(Player{50,50,PieceColor::Red}) },
+	m_player2{ std::make_shared<Player>(Player{50,50,PieceColor::Black}) },
+	m_current_player{ m_player1.get() }
+{}
+
+const Board& Game::getBoard() const noexcept
+{
+	return m_board;
+}
+
+Player* Game::getCurrentPlayer() const noexcept
+{
+	return m_current_player;
+}
+
+bool Game::finished()
+{
+	//De implementat verificare daca jocul sa terminat;
+	return false;
+}
+
+void Game::nextPlayer()
+{
+	if (m_current_player == m_player1.get()) {
+		m_current_player = m_player2.get();
+	}
+	else {
+		m_current_player = m_player1.get();
+	}
+}
+
+void Game::playMove(Point move)
+{
+	m_board.addPillar(move);
+}
