ac65309b1da2bb9b4e881df88511e666cae68090
GrandPaPaRa <tomaghghgh@gmail.com>
2023-11-30 16:45:52 +0200

    create game class

---
+++ TwixT/TwixT/Game.h
@@ -0,0 +1,21 @@
+#pragma once
+#include"Player.h"
+#include"Board.h"
+
+class Game : public GameElement
+{
+protected:
+	Board m_board;
+public:
+	Game();
+	const Board& getBoard() const noexcept;
+	Player* getCurrentPlayer() const noexcept;
+	void nextPlayer();
+	bool finished();
+	void playMove(Point move);
+private:
+	std::shared_ptr<Player> m_player1;
+	std::shared_ptr<Player> m_player2;
+	Player* m_current_player;
+};
+
