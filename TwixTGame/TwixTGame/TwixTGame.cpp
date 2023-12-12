5b76104f0a2692016fef628df413c76b196ca714
Secareanu David <david.secareanu@student.unitbv.ro>
2023-12-13 01:52:57 +0200

    update twixtgame class

--- TwixTGame/TwixTGame/TwixTGame.cpp
+++ TwixTGame/TwixTGame/TwixTGame.cpp
@@ -9,6 +9,7 @@ TwixTGame::TwixTGame()
 
 void TwixTGame::run()
 {
+	m_camera.show();
 }
 
 void TwixTGame::switchScreen()
@@ -19,11 +20,18 @@ void TwixTGame::backToMyMenu()
 
 void TwixTGame::play()
 {
+	m_game_scene->newGame();
+	m_camera.setScene(m_game_scene.get());
 }
 
 void TwixTGame::exit()
 {
+	m_camera.close();
 }
 
 void TwixTGame::backToMainMenu()
-{}
+{
+	m_camera.setScene(&m_main_menu);
+	m_main_menu.update();
+	std::unique_ptr<GameScene> m_game_scene{ std::make_unique<GameScene>(this) };
+}
