3c348d5126205e35832a94975491cb72b216cc11
Secareanu David <david.secareanu@student.unitbv.ro>
2023-12-13 01:44:30 +0200

    adaugare functionalitate si populare scena

--- TwixTGame/TwixTGame/MainMenu.cpp
+++ TwixTGame/TwixTGame/MainMenu.cpp
@@ -10,8 +10,18 @@ m_fundal{ QPixmap{ "../assets/fundal.png" } }
     m_buttons[1] = std::make_unique<Button>("../assets/load", QPoint{ 111,260 });
     m_buttons[2] = std::make_unique<Button>("../assets/screen", QPoint{ 80,330 });
     m_buttons[3] = std::make_unique<Button>("../assets/exit", QPoint{ 122,400 });
+    //adaugam functiionalitatea butoanelor;
+    QObject::connect(m_buttons[0].get(), &Button::buttonClicked, static_cast<TwixTGame*>(parent), &TwixTGame::play);
+    QObject::connect(m_buttons[2].get(), &Button::buttonClicked, static_cast<TwixTGame*>(parent), &TwixTGame::switchScreen);
+    QObject::connect(m_buttons[3].get(), &Button::buttonClicked, static_cast<TwixTGame*>(parent), &TwixTGame::exit);
+    populateScene();
 }
 
 void MainMenu::populateScene()
 {
+    addItem(&m_fundal);
+    for (const auto& button : m_buttons)
+    {
+        addItem(button.get());
+    }
 }
