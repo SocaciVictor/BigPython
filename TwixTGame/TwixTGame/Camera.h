5c05726a607b89cb56be7b496dc491d392593c16
Secareanu David <david.secareanu@student.unitbv.ro>
2024-01-12 19:56:02 +0200

    delete

--- TwixTGame/TwixTGame/Camera.h
+++
@@ -1,9 +0,0 @@
-//#pragma once
-//#include<QGraphicsView>
-//
-//class Camera : public QGraphicsView
-//{
-//public:
-//	Camera(int width, int height, QWidget* parent = nullptr);
-//};
-//
--- TwixTGame/TwixTGame/SecondMenu.cpp
+++
@@ -1,1 +0,0 @@
-//#include "SecondMenu.h"
